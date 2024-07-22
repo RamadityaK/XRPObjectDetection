8a33df8be35138d891408aaade1661f77893a91d
RamadityaK <adi.k394@yahoo.com>
2024-07-22 10:57:53 -0500

    Added comments and cleaned up the code

--- Detect/SerialObjectDetection.py
+++ Detect/SerialObjectDetection.py
@@ -24,28 +24,31 @@ ser = serial.Serial(
     timeout=1
     )
 
-
-last_time = time.time()
-base_options = python.BaseOptions(model_asset_path = 'basket.tflite')
+base_options = python.BaseOptions(model_asset_path = 'efficientdet.tflite')
 options = vision.ObjectDetectorOptions(base_options=base_options,
-                                       score_threshold=0.3)
+                                       score_threshold=0.4)
 detector = vision.ObjectDetector.create_from_options(options)
-target = 'basket'
+
+#Set Variables
+last_time = 0
+inference_time = 0
+target = 'person'
 
 
 while True:
-
-    
+    #Grab an image and format it
     image = picam2.capture_array()
-    image = cv2.rotate(image, cv2.ROTATE_180)
-    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-
+    image = cv2.rotate(image, cv2.ROTATE_180) #Change or omit this based on your camera orientation
+    rgb_image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB) #Converts image from BGR to RGB format
     #rgb_image = cv2.resize(rgb_image,(640,640))
-    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image)
+    mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb_image) #Reformat to Mediapipe format
 
+    #Run inferencing and time it for benchmarking
+    last_time = time.time()
     detection_result = detector.detect(mp_image)
-#     print(detection_result)
-#     print('-----------------------')
+    inference_time = time.time() - last_time
+
+    #Initialize Loop Variables
     centroidx = 0
     centroidy = 0
     deviationx = 0
@@ -53,9 +56,12 @@ while True:
     width = 0
     height = 0
     detected = 0
+
+    #Parse through detections
     for detection in detection_result.detections:
         category = detection.categories[0]
         classification = category.category_name
+        #Only execute if the target class matches the detected class
         if(classification == target):
             print(str(round(category.score,2)))
             bbox = detection.bounding_box
@@ -72,21 +78,14 @@ while True:
             #Write Relevant Variables over the Serial Port
         
             #Print to console for Debugging
-            #print(category.category_name, ", with prob: ", str(round(category.score,2))
-                  #, "and centroid: ", str(centroidx),",",str(centroidy))
+            #print(category.category_name, ", with prob: ", str(round(category.score,2)), "and centroid: ", str(centroidx),",",str(centroidy))
             #print('deviation from center: ', str(deviationx), ",", str(deviationy))
-    #
+    
+    #Write our output over the serial port
     output = str(detected) + ',' + str(deviationx) + ',' + str(deviationy) + ',' + str(width) + ',' +str(height)
     ser.write(output.encode())
+
     #The written data should be of the following format:
     # detected?(1 or 0), xdeviation, ydeviation, box width, box height.
     print(output)
     #print("Inference Time: ", str(time.time()-last_time))
-    last_time = time.time()
-        
-#image_copy = np.copy(image.numpy_view())
-# annotated_image = visualize(image_copy,detection_result)
-# rgb_annotated_image = cv2.cvtColor(annotated_image, cv2.COLOR_BGR2RGB)
-# cv2.imshow(rgb_annotated_image)
-
-cv2.destroyAllWindows()
